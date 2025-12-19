41548c5a7cef201ea336ac085a125b9496c69fbc
Anant Jain <ajain18@dt-login03.delta.ncsa.illinois.edu>
2025-12-19 09:05:19 -0600

    remaining

--- build/CMakeFiles/result.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/result"
-  "jacobi2d.out"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/result.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
